c4535d4b54e1ca0f68e80363b76707e4276e696d
manoj sai <manojsai.l2004@gmail.com>
2024-12-02 15:16:25 +0530

    feat: add band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+code_name = input("What's your code name?\n")
+
+print(f"Your band name could be {city_name} {code_name}")
