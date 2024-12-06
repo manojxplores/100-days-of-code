b5d6e3ae84766823aa6eb3124deff2e0d8bb65d6
manoj sai <manojsai.l2004@gmail.com>
2024-12-06 20:54:25 +0530

    chore: rename folders to include project day

--- 02-tip-calculator/tip_calculator.py
+++ 02-tip-calculator/tip_calculator.py
@@ -1,4 +1,4 @@
-print("Welcome to the tip calculator!")
+print("Welcome to the tip 10calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 num = int(input("How many people to split the bill?"))
