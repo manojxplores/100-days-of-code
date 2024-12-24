bcf0cc05f60f6933b6c02b29e9f3b549b023c3d4
manoj sai <manojsai.l2004@gmail.com>
2024-12-24 22:45:47 +0530

    fix: input validation

--- Intermediate/26-NATO-alphabet/main.py
+++ Intermediate/26-NATO-alphabet/main.py
@@ -3,6 +3,17 @@ import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
-user_input = input("Enter a word: ").upper()
-word_list = [nato_dict[letter] for letter in user_input if letter in nato_dict.keys()]
-print(word_list)
+
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        word_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(word_list)
+
+
+generate_phonetic()
