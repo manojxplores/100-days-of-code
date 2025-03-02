26cab256251b2b18321ff18399e6c7a3d8cc81ce
manoj sai <manojsai.l2004@gmail.com>
2025-03-02 22:32:06 +0530

    fix: refactor code

--- Intermediate+/53-automate-data-entry/main.py
+++ Intermediate+/53-automate-data-entry/main.py
@@ -13,15 +13,24 @@ chrome_options.add_experimental_option("detach", True)
 
 soup = ScrapeData(content)
 soup.get_data()
-data_list = [soup.address_list, soup.price_list, soup.a_list]
+all_addresses = soup.address_list
+all_prices = soup.price_list
+all_links = soup.a_list
 
-for i in range(len(soup.address_list)):
+for i in range(len(all_prices)):
+
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://forms.gle/m6qiiU48aK7io2Bn7")
     time.sleep(2)
+
     input_divs = driver.find_elements(By.CSS_SELECTOR, value="input.whsOnd.zHQkBf")
-    for idx, div in enumerate(input_divs):
-        div.send_keys(data_list[idx][i])
+    address = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    anchor = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+
+    address.send_keys(soup.address_list[i])
+    price.send_keys(soup.price_list[i])
+    anchor.send_keys(soup.a_list[i])
 
     btn = driver.find_element(By.CSS_SELECTOR, value="span.NPEfkd.RveJvd.snByac")
     btn.click()
